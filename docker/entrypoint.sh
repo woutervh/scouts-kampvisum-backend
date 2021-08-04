dd0a85d3fbe3f8dd7a9b6be040aed341655518dc
sverholen <sverholen@gmail.com>
2021-08-04 20:52:08 +0200

    Emptied entrypoint.sh

--- docker/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-RUN command -v ssh-agent >/dev/null || ( apt-get update -y && apt-get install openssh-client -y )'
-RUN eval $(ssh-agent -s)
-RUN echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add -
-RUN mkdir -p ~/.ssh
-RUN chmod 700 ~/.ssh
-RUN git config --global user.email "boro@inuits.eu"
-RUN git config --global user.name "inuits"
