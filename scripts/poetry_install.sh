01a9cd9495904dc377b5eaa4c13b27f1bb9552b9
sverholen <sverholen@gmail.com>
2021-08-04 08:47:50 +0200

    Updated poetry script and added generation of requirements.txt

---
+++ scripts/poetry_install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Generated at http://patorjk.com/software/taag/
+cat << "EOF"
+##############################################################################
+##############################################################################
+###                                                                        ###
+###                                                                        ###
+###                       _             _ _                                ###
+###                      (_)_ __  _   _(_) |_ ___                          ###
+###                      | | '_ \| | | | | __/ __|                         ###
+###                      | | | | | |_| | | |_\__ \                         ###
+###                      |_|_| |_|\__,_|_|\__|___/                         ###
+###                                                                        ###
+###                                                                        ###
+##############################################################################
+##############################################################################
+EOF
+
+# https://python-poetry.org/docs/
+# https://nanthony007.medium.com/stop-using-pip-use-poetry-instead-db7164f4fc72
+
+# Using pip to install poetry avoids the need to pipe in dependencies in
+# a docker.
+python3.9 -m pip install poetry
+
+# Check if it is installed correctly: poetry -version
+
+# Run poetry init, setup basic information, and add dependencies:
+# Project dependencies:
+poetry add django_rest_framework
+poetry add mozilla_django_oidc
+poetry add drf_yasg2
+poetry add cffi
+poetry add django-cors-headers
+
+# Let poetry download all packages and generate the poetry.lock file
+poetry install
+
+# Generate the requirements.txt
+poetry export -f requirements.txt --output requirements.txt
+
+
