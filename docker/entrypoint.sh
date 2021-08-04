bd8f08e3d8334ce4a2e71424ef36d8e269da64f0
sverholen <sverholen@gmail.com>
2021-08-04 20:26:34 +0200

    Make docker recognize ssh key for scouts-auth

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,47 +1,7 @@
-### Base stage
-FROM python:3.7-slim AS base-stage
-
-ENV PYTHONFAULTHANDLER=1 \
-    PYTHONHASHSEED=random \
-    PYTHONUNBUFFERED=1 \
-    BASE_DIR=/app
-
-RUN adduser --no-create-home --disabled-password --home /app --shell /bin/ash app
-WORKDIR /app
-
-COPY docker/entrypoint*.sh /
-COPY scouts_kampvisum_api/manage.py ./
-COPY scouts_kampvisum_api/apps ./apps
-
-ENTRYPOINT ["/entrypoint.sh"]
-
-### Preparation stage
-FROM base-stage AS preparation-stage
-
-ARG APP_ENV
-
-ENV APP_ENV=${APP_ENV} \
-    PIP_DEFAULT_TIMEOUT=100 \
-    PIP_DISABLE_PIP_VERSION_CHECK=1 \
-    PIP_NO_CACHE_DIR=1 \
-    POETRY_VERSION=1.1.4
-
-RUN apt-get update; \
-    apt-get install -y --no-install-recommends \
-        gcc \
-        libpq-dev \
-        libffi-dev \
-        python3-dev \
-        default-libmysqlclient-dev
-
-RUN pip install "poetry==${POETRY_VERSION}"
-
-COPY pyproject.toml poetry.lock* ./
-
-### Development stage
-FROM preparation-stage AS development-stage
-
-RUN poetry config virtualenvs.create false
-RUN poetry install
-
-CMD ["--no-migrate"]
+RUN command -v ssh-agent >/dev/null || ( apt-get update -y && apt-get install openssh-client -y )'
+RUN eval $(ssh-agent -s)
+RUN echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add -
+RUN mkdir -p ~/.ssh
+RUN chmod 700 ~/.ssh
+RUN git config --global user.email "boro@inuits.eu"
+RUN git config --global user.name "inuits"
