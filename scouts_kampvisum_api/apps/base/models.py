0f036b57541467feee98de65d44f165338c3c556
sverholen <sverholen@gmail.com>
2021-08-10 16:25:27 +0200

    Fixed missing uuid with pre_save signal

--- scouts_kampvisum_api/apps/base/models.py
+++ scouts_kampvisum_api/apps/base/models.py
@@ -1,5 +1,7 @@
 import uuid
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from safedelete.models import SafeDeleteModel
 from safedelete.models import SOFT_DELETE
 
@@ -10,8 +12,10 @@ class BaseModel(SafeDeleteModel):
     
     _safedelete_policy = SOFT_DELETE
     
-    id = models.UUIDField(
-        primary_key=True, editable = False, default=uuid.uuid4)
+    id = models.AutoField(
+        primary_key=True, editable=False)
+    uuid = models.UUIDField(
+        primary_key=False, editable=False, default=uuid.uuid4, unique=True)
     
     class Meta:
         abstract = True
@@ -19,4 +23,10 @@ class BaseModel(SafeDeleteModel):
 
 class ScoutsGroup(ScoutsAuthGroup):
     pass
+
+
+@receiver(pre_save, sender=BaseModel)
+def set_uuid_on_save(sender, instance, *args, **kwargs):
+    if instance.pk is None:
+        instance.uuid = uuid.uuid4()
 
--- scouts_kampvisum_api/apps/base/signals.py
+++ scouts_kampvisum_api/apps/base/signals.py
@@ -1,3 +1,5 @@
 #https://stackoverflow.com/questions/25724318/django-fixtures-save-with-default-value
 #https://docs.djangoproject.com/en/3.2/topics/signals/#django.dispatch.receiver
 
+
+
