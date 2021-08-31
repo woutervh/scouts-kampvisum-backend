effcc9dfdb6703ae8d2e29130206ec305a51847e
sverholen <sverholen@gmail.com>
2021-08-31 11:13:14 +0200

    Keep existing keys in object representation

--- scouts_kampvisum_api/inuits/mixins.py
+++ scouts_kampvisum_api/inuits/mixins.py
@@ -51,7 +51,7 @@ class FlattenMixin(object):
 
         if hasattr(self.Meta, 'flatten'):
             for field, serializer_class in self.Meta.flatten:
-                serializer = serializer_class(context = self.context)
+                serializer = serializer_class(context=self.context)
                 objrep = serializer.to_representation(getattr(obj, field))
 
                 for key in objrep:
@@ -59,10 +59,12 @@ class FlattenMixin(object):
                     #     raise ValidationError(
                     #         "A field with name '" + key + "' already exists")
                     # representation[field + "__" + key] = objrep[key]
-                    representation[key] = objrep[key]
-        
+                    # representation[key] = objrep[key]
+                    if not key in representation:
+                        representation[key] = objrep[key]
+
         return representation
-    
+
     def to_internal_value(self, data):
 
         # remove flattened nested keys
@@ -76,10 +78,10 @@ class FlattenMixin(object):
                     if key in data:
                         serializer_internal[key] = data.pop(key)
                 nested_fields[field] = serializer_internal
-        
+
         internal_values = super().to_internal_value(data)
         for key in nested_fields:
             internal_values[key] = nested_fields[key]
         logger.debug("INTERNAL VALUES: %s", internal_values)
-        
+
         return internal_values
