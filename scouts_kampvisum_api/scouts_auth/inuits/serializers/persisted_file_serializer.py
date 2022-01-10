6ab5cf1832fd71c5a678bc972a4b7d9bc883c3db
sverholen <sverholen@gmail.com>
2022-01-10 18:52:19 +0100

    Return file url with PersistedFile

--- scouts_kampvisum_api/scouts_auth/inuits/serializers/persisted_file_serializer.py
+++ scouts_kampvisum_api/scouts_auth/inuits/serializers/persisted_file_serializer.py
@@ -1,3 +1,4 @@
+from django.core.files.storage import get_storage_class
 from rest_framework import serializers
 
 from scouts_auth.inuits.models import PersistedFile
@@ -6,12 +7,17 @@ from scouts_auth.inuits.models import PersistedFile
 class PersistedFileSerializer(serializers.ModelSerializer):
 
     id = serializers.UUIDField()
+    url = serializers.SerializerMethodField()
     name = serializers.SerializerMethodField()
     size = serializers.SerializerMethodField()
 
     class Meta:
         model = PersistedFile
         fields = "__all__"
+
+    def get_url(self, obj: PersistedFile):
+        if obj and hasattr(obj, "file") and obj.file and hasattr(obj.file, "name"):
+            return get_storage_class()().url(obj.file.name)
 
     def get_name(self, obj: PersistedFile):
         if obj and hasattr(obj, "file") and obj.file and hasattr(obj.file, "name"):
