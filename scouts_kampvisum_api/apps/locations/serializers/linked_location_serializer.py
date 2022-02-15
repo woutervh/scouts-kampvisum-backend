6e4f4c0b28daa54f21f50bcb7c4e42ee0d435e0f
sverholen <sverholen@gmail.com>
2022-02-15 12:03:58 +0100

    Fixes to location check

--- scouts_kampvisum_api/apps/locations/serializers/linked_location_serializer.py
+++ scouts_kampvisum_api/apps/locations/serializers/linked_location_serializer.py
@@ -1,7 +1,12 @@
+import logging
+
 from rest_framework import serializers
 
 from apps.locations.models import LinkedLocation
 from apps.locations.serializers import CampLocationSerializer
+
+
+logger = logging.getLogger(__name__)
 
 
 class LinkedLocationSerializer(serializers.ModelSerializer):
@@ -11,3 +16,10 @@ class LinkedLocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = LinkedLocation
         fields = "__all__"
+
+    def to_internal_value(self, data: dict) -> dict:
+        logger.debug("LINKED LOCATION SERIALIZER TO_INTERNAL_VALUE: %s", data)
+
+        data = super().to_internal_value(data)
+
+        return data
