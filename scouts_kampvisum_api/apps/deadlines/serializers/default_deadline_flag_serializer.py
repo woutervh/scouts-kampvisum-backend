845e0585503826a2b2fb95f8ef696db182d92196
sverholen <sverholen@gmail.com>
2022-02-15 20:59:25 +0100

    Save all before commit

---
+++ scouts_kampvisum_api/apps/deadlines/serializers/default_deadline_flag_serializer.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from apps.deadlines.models import DefaultDeadlineFlag
+
+
+class DefaultDeadlineFlagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DefaultDeadlineFlag
+        exclude = ["default_deadline", "flag"]
