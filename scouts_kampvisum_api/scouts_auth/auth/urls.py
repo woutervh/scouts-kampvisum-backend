96084f4c253c13f97b7f113d44a24051c687ea2c
sverholen <sverholen@gmail.com>
2022-04-08 08:37:35 +0200

    feat #92930 add permissions call for group and latest insomnia

--- scouts_kampvisum_api/scouts_auth/auth/urls.py
+++ scouts_kampvisum_api/scouts_auth/auth/urls.py
@@ -2,15 +2,23 @@ from django.urls import path
 
 from scouts_auth.auth.views import (
     CurrentUserView,
-    PermissionsView,
+    PermissionsViewSet,
     OIDCAuthCodeView,
     OIDCRefreshView,
 )
 
+permissions = PermissionsViewSet.as_view({"get": "get"})
+permissions_for_group = PermissionsViewSet.as_view({"get": "get_for_group"})
+
 urlpatterns = [
     # The infamous 'me' call
     path("auth/me/", CurrentUserView.as_view(), name="me"),
-    path("auth/permissions/", PermissionsView.as_view(), name="permissions"),
+    path("auth/permissions/", permissions, name="permissions"),
+    path(
+        "auth/permissions/<str:group_admin_id>",
+        permissions_for_group,
+        name="permissions_for_group",
+    ),
     # Authenticate with OIDC
     path("oidc/token/", OIDCAuthCodeView.as_view(), name="token"),
     # Refresh the OIDC authentication
--- scouts_kampvisum_api/scouts_auth/auth/views/permissions_view.py
+++ scouts_kampvisum_api/scouts_auth/auth/views/permissions_view.py
@@ -1,11 +1,12 @@
-from rest_framework import views, permissions, status
+from rest_framework import viewsets, status, serializers
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from drf_yasg2.utils import swagger_auto_schema
 
 from scouts_auth.auth.models import User
-from scouts_auth.auth.serializers import UserSerializer
 
-from scouts_auth.groupadmin.services import GroupAdmin
+from scouts_auth.groupadmin.models import ScoutsGroup
+from scouts_auth.groupadmin.services import ScoutsAuthorizationService
 
 
 # LOGGING
@@ -15,14 +16,34 @@ from scouts_auth.inuits.logging import InuitsLogger
 logger: InuitsLogger = logging.getLogger(__name__)
 
 
-class PermissionsView(views.APIView):
-    permission_classes = [permissions.IsAuthenticated]
-    service = GroupAdmin()
+class PermissionsViewSet(viewsets.GenericViewSet):
+    permission_classes = [IsAuthenticated]
 
-    @swagger_auto_schema(responses={status.HTTP_200_OK: UserSerializer})
+    authorization_service = ScoutsAuthorizationService()
+
+    @swagger_auto_schema(responses={status.HTTP_200_OK: serializers.Serializer})
     def get(self, request):
         try:
             user: User = request.user
+
+            return Response(user.permissions)
+        except Exception as exc:
+            logger.error("SCOUTS_AUTH: Error while getting user permissions", exc)
+
+    @swagger_auto_schema(responses={status.HTTP_200_OK: serializers.Serializer})
+    def get_for_group(self, request, group_admin_id: str):
+        try:
+            user: User = request.user
+            group: ScoutsGroup = ScoutsGroup.objects.safe_get(
+                group_admin_id=group_admin_id, raise_error=True
+            )
+
+            # HACKETY HACK
+            # This should probably be handled by a rest call when changing groups in the frontend,
+            # but adding it here avoids the need for changes to the frontend
+            self.authorization_service.update_user_authorizations(
+                user=request.user, scouts_group=group
+            )
 
             return Response(user.permissions)
         except Exception as exc:
