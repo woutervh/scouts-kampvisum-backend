1337b3497a9866715e30ad9a2fa609a67a2b2586
Stijn Verholen <boro@inuits.eu>
2022-09-21 10:12:57 +0200

    fix: add missing categories for previous camp years

--- scouts_kampvisum_api/apps/camps/management/commands/setupcampyears.py
+++ scouts_kampvisum_api/apps/camps/management/commands/setupcampyears.py
@@ -1,3 +1,8 @@
+import os
+from pathlib import Path
+
+from django.conf import settings
+from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
 
@@ -12,7 +17,18 @@ class Command(BaseCommand):
     help = "Sets up the camp years"
     exception = False
 
+    BASE_PATH = "apps/camps/fixtures"
+    FIXTURE = "camp_years.json"
+
     def handle(self, *args, **kwargs):
+        parent_path = Path(settings.BASE_DIR)
+
+        data_path = "{}/{}".format(self.BASE_PATH, self.FIXTURE)
+        path = os.path.join(parent_path, data_path)
+
+        logger.debug("LOADING camp year fixture %s", path)
+        call_command("loaddata", path) 
+
         from apps.camps.services import CampYearService
 
         logger.debug("Setting up camp years")
