e846f876c9f6836c4c32b177ab566ca7282bfd46
Stijn Verholen <boro@inuits.eu>
2023-01-27 14:37:28 +0100

    fix: location serializer

--- scouts_kampvisum_api/apps/locations/serializers/linked_location_serializer.py
+++ scouts_kampvisum_api/apps/locations/serializers/linked_location_serializer.py
@@ -13,18 +13,16 @@ logger: InuitsLogger = logging.getLogger(__name__)
 
 class LinkedLocationSerializer(serializers.ModelSerializer):
 
-    locations = CampLocationSerializer(many=True, read_only=True)
+    locations = CampLocationSerializer(many=True)
 
     class Meta:
         model = LinkedLocation
-        # fields = ["name", "contact_name", "contact_phone", "contact_email", "is_camp_location",
-        #           "center_latitude", "center_longitude", "zoom", "start_date", "end_date", "locations"]
         fields = "__all__"
 
     def to_internal_value(self, data: dict) -> dict:
         # logger.debug("LINKED LOCATION SERIALIZER TO_INTERNAL_VALUE: %s", data)
 
-        id = data.get("id", None)
+        pk = data.get("id", None)
         if id and len(data.keys()) == 1:
             instance = LinkedLocation.objects.safe_get(id=id)
             if instance:
@@ -32,7 +30,7 @@ class LinkedLocationSerializer(serializers.ModelSerializer):
 
         data = super().to_internal_value(data)
 
-        data["id"] = id
+        data["id"] = pk
 
         return data
 
