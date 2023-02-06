06ba9820f27ae1795254db991803ee13234c8be4
Stijn Verholen <boro@inuits.eu>
2023-02-06 10:23:26 +0100

    fix: clean migrations

--- scouts_kampvisum_api/scouts_auth/migrations/0001_initial.py
+++ scouts_kampvisum_api/scouts_auth/migrations/0001_initial.py
@@ -24,15 +24,24 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='ScoutsFunction',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('group_admin_id', scouts_auth.groupadmin.models.fields.group_admin_id_field.GroupAdminIdField(max_length=48)),
-                ('begin', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalDateTimeField(blank=True, null=True)),
-                ('end', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalDateTimeField(blank=True, null=True)),
-                ('code', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalCharField(blank=True, default='', max_length=128)),
-                ('description', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalCharField(blank=True, default='', max_length=128)),
-                ('type', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalCharField(blank=True, default='', max_length=128)),
-                ('max_birth_date', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalDateField(blank=True, null=True)),
-                ('adjunct', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalCharField(blank=True, default='', max_length=128)),
+                ('id', models.AutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
+                ('group_admin_id', scouts_auth.groupadmin.models.fields.group_admin_id_field.GroupAdminIdField(
+                    max_length=48)),
+                ('begin', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalDateTimeField(
+                    blank=True, null=True)),
+                ('end', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalDateTimeField(
+                    blank=True, null=True)),
+                ('code', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalCharField(
+                    blank=True, default='', max_length=128)),
+                ('description', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalCharField(
+                    blank=True, default='', max_length=128)),
+                ('type', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalCharField(
+                    blank=True, default='', max_length=128)),
+                ('max_birth_date', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalDateField(
+                    blank=True, null=True)),
+                ('adjunct', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalCharField(
+                    blank=True, default='', max_length=128)),
             ],
             options={
                 'managed': False,
@@ -41,14 +50,22 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='ScoutsGroup',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('group_admin_id', scouts_auth.groupadmin.models.fields.group_admin_id_field.GroupAdminIdField(max_length=48)),
-                ('number', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalCharField(blank=True, default='', max_length=128)),
-                ('name', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalCharField(blank=True, default='', max_length=128)),
-                ('email', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalEmailField(blank=True, default='', max_length=254, null=True)),
-                ('website', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalCharField(blank=True, default='', max_length=128)),
-                ('parent_group', scouts_auth.groupadmin.models.fields.group_admin_id_field.GroupAdminIdField(max_length=48)),
-                ('type', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalCharField(blank=True, default='', max_length=128)),
+                ('id', models.AutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
+                ('group_admin_id', scouts_auth.groupadmin.models.fields.group_admin_id_field.GroupAdminIdField(
+                    max_length=48)),
+                ('number', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalCharField(
+                    blank=True, default='', max_length=128)),
+                ('name', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalCharField(
+                    blank=True, default='', max_length=128)),
+                ('email', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalEmailField(
+                    blank=True, default='', max_length=254, null=True)),
+                ('website', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalCharField(
+                    blank=True, default='', max_length=128)),
+                ('parent_group', scouts_auth.groupadmin.models.fields.group_admin_id_field.GroupAdminIdField(
+                    max_length=48)),
+                ('type', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalCharField(
+                    blank=True, default='', max_length=128)),
             ],
             options={
                 'managed': False,
@@ -57,25 +74,39 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='ScoutsUser',
             fields=[
-                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False, unique=True)),
-                ('username', models.CharField(error_messages={'unique': 'A user with that username already exists.'}, help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.', max_length=150, unique=True, validators=[django.contrib.auth.validators.UnicodeUsernameValidator])),
+                ('is_superuser', models.BooleanField(default=False,
+                 help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
+                ('id', models.UUIDField(default=uuid.uuid4, editable=False,
+                 primary_key=True, serialize=False, unique=True)),
+                ('username', models.CharField(error_messages={'unique': 'A user with that username already exists.'},
+                 help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.', max_length=150, unique=True, validators=[django.contrib.auth.validators.UnicodeUsernameValidator])),
                 ('first_name', models.CharField(blank=True, max_length=150)),
                 ('last_name', models.CharField(blank=True, max_length=150)),
-                ('email', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalEmailField(blank=True, default='', max_length=254, null=True)),
-                ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.')),
-                ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.')),
-                ('date_joined', models.DateTimeField(default=django.utils.timezone.now)),
+                ('email', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalEmailField(
+                    blank=True, default='', max_length=254, null=True)),
+                ('is_staff', models.BooleanField(default=False,
+                 help_text='Designates whether the user can log into this admin site.')),
+                ('is_active', models.BooleanField(
+                    default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.')),
+                ('date_joined', models.DateTimeField(
+                    default=django.utils.timezone.now)),
                 ('password', models.CharField(max_length=128)),
                 ('last_login', models.DateTimeField(blank=True, null=True)),
-                ('group_admin_id', scouts_auth.groupadmin.models.fields.group_admin_id_field.GroupAdminIdField(max_length=48)),
-                ('gender', scouts_auth.inuits.models.fields.django_shorthand_model_fields.DefaultCharField(blank=True, choices=[('F', 'Female'), ('I', 'Mixed'), ('M', 'Male'), ('X', 'Other'), ('U', 'Unknown')], default='U', max_length=16)),
-                ('phone_number', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalCharField(blank=True, default='', max_length=48)),
-                ('membership_number', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalCharField(blank=True, default='', max_length=48)),
-                ('customer_number', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalCharField(blank=True, default='', max_length=48)),
+                ('group_admin_id', scouts_auth.groupadmin.models.fields.group_admin_id_field.GroupAdminIdField(
+                    max_length=48)),
+                ('gender', scouts_auth.inuits.models.fields.django_shorthand_model_fields.DefaultCharField(blank=True, choices=[
+                 ('F', 'Female'), ('I', 'Mixed'), ('M', 'Male'), ('X', 'Other'), ('U', 'Unknown')], default='U', max_length=16)),
+                ('phone_number', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalCharField(
+                    blank=True, default='', max_length=48)),
+                ('membership_number', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalCharField(
+                    blank=True, default='', max_length=48)),
+                ('customer_number', scouts_auth.inuits.models.fields.django_shorthand_model_fields.OptionalCharField(
+                    blank=True, default='', max_length=48)),
                 ('birth_date', models.DateField(blank=True, null=True)),
-                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
-                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),
+                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+                 related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
+                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.',
+                 related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),
             ],
             options={
                 'abstract': False,
@@ -87,14 +118,21 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='PersistedFile',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
-                ('created_on', models.DateTimeField(default=django.utils.timezone.now)),
-                ('updated_on', models.DateTimeField(default=django.utils.timezone.now)),
-                ('original_name', scouts_auth.inuits.models.fields.django_shorthand_model_fields.RequiredCharField(max_length=128)),
-                ('file', models.FileField(blank=True, null=True, upload_to='', validators=[scouts_auth.inuits.files.validators.validate_uploaded_file])),
+                ('id', models.UUIDField(default=uuid.uuid4,
+                 editable=False, primary_key=True, serialize=False)),
+                ('created_on', models.DateTimeField(
+                    default=django.utils.timezone.now)),
+                ('updated_on', models.DateTimeField(
+                    default=django.utils.timezone.now)),
+                ('original_name', scouts_auth.inuits.models.fields.django_shorthand_model_fields.RequiredCharField(
+                    max_length=128)),
+                ('file', models.FileField(blank=True, null=True, upload_to='', validators=[
+                 scouts_auth.inuits.files.validators.validate_uploaded_file])),
                 ('content_type', models.CharField(max_length=100)),
-                ('created_by', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='%(app_label)s_%(class)s_created', to=settings.AUTH_USER_MODEL, verbose_name='Created by')),
-                ('updated_by', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='%(app_label)s_%(class)s_updated', to=settings.AUTH_USER_MODEL, verbose_name='updated by')),
+                ('created_by', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL,
+                 related_name='%(app_label)s_%(class)s_created', to=settings.AUTH_USER_MODEL, verbose_name='Created by')),
+                ('updated_by', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL,
+                 related_name='%(app_label)s_%(class)s_updated', to=settings.AUTH_USER_MODEL, verbose_name='updated by')),
             ],
             options={
                 'abstract': False,
